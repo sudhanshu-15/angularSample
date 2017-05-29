82b68b8d9cd8b485d2a96d269debbe746fca6151
Sudhanshu Siddh <sudhanshu@tarfite.local>
2017-05-29 02:31:25 -0400

    Angular sample project initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlert } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlert,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+    serverId: Number = 10;
+    serverStatus: String = "Offline";
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+}
